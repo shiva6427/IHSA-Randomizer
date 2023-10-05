a04633bbd18ddcb7f4cf8948a7ada0b195858744
Shiva Vangari <49754508+shiva6427@users.noreply.github.com>
2023-10-05 18:06:25 -0500

    Updated files

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import RandomizePage from './RandomizePage'; // Adjust the import path as needed
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <RandomizePage /> {/* Render the RandomizePage component */}
+    </div>
+  );
+}
+
+export default App;
